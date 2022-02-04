5394e5ad90341faa43c324a9a3fef557b3a0c9f9
auz <lankyauz@gmail.com>
2022-02-04 12:38:57 -0700

    The script can now take user input and compare it to the computers random choice to play a single game.
    Next I'd like to check the users input and only allow the game to finish if they've input one of the 3 valid choices.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 // computer makes a selection at random
 // math floor rounds a number down to the nearest whole integer
-function computerSelection(){
+// math random returns a float from 0 to 0.99 *
+function computerPlay(){
     let selection = Math.floor(Math.random()*3);
     if(selection==0){
         return 'rock';
@@ -14,9 +15,38 @@ function computerSelection(){
         return 'scissors';
     }
 }
-console.log(computerSelection());
-// I make a selection
+let computerSelection = computerPlay();
 
-// the 2 selections are then compared
-// a winner is declared
+// the 2 selections are then compared, and a winner is declared.
+function outcome(){
+    // console.log(computerSelection); <-for testing
+    let playerSelection = window.prompt('rock, paper, or scissors?');
+    let input = playerSelection.toLowerCase();
+
+    console.log(`You have chosen ${input}.`);
+    console.log(`The computer has chosen ${computerSelection}.`)
+
+    if(input===computerSelection){
+        return console.log('It\'s a tie.');
+    }
+    else if(input==='rock' && computerSelection==='paper'){
+        return console.log('Computer wins.');
+    }
+    else if(input==='rock' && computerSelection==='scissors'){
+        return console.log('User wins.');
+    }
+    else if(input==='paper' && computerSelection==='rock'){
+        return console.log('User wins.');
+    }
+    else if(input==='paper' && computerSelection==='scissors'){
+        return console.log('Computer wins.');
+    }
+    else if(input==='scissors' && computerSelection==='rock'){
+        return console.log('Computer wins.');
+    }
+    else{
+        return console.log('User wins.');
+    }
+}
+outcome();
 // play 5 games then stop
