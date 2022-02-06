2007997e9ba3d2309335e1bae627fd9e0286a2fa
auz <lankyauz@gmail.com>
2022-02-06 12:10:02 -0700

    The project page said to use both selections as parameters in the playRound function, but it seems better to call them in the function. Hopefully once I take a look at others code it'll make more sense.
    Can now play 5 games in a row.
    
    Need to be able to keep score next.

--- script.js
+++ script.js
@@ -6,73 +6,103 @@
 function computerPlay(){
     let selection = Math.floor(Math.random()*3);
     if(selection==0){
+        console.log('The computer has chosen rock.');
         return 'rock';
     }
     else if(selection==1){
+        console.log('The computer has chosen paper.');
         return 'paper';
     }
     else{
+        console.log('The computer has chosen scissors.');
         return 'scissors';
     }
 }
 
-// computer selection and player selection
-let computerSelection = computerPlay();
-let playerSelection = window.prompt('rock, paper or scissors?');
-
+// create a player selection function that limits out input to one of the 3 desired choices.
+// sadly it seems we may not beable to limit input with window.prompt
+// function playerSelection(){
+//     let rawInput = window.prompt('rock, paper, or scissors?');
+//     let userInput = rawInput.toLowerCase();
+//     if(userInput==='rock'){
+//         console.log('You have chosen rock.');
+//         return 'rock';
+//     }
+//     else if(userInput==='paper'){
+//         console.log('You have chosen paper.');
+//         return 'paper';
+//     }
+//     else if(userInput==='scissors'){
+//         console.log('You have chosen scissors.');
+//         return 'scissors';
+//     }
+//     else{
+//         console.log('Invalid input, try again.');
+//         playerSelection();
+//     }
+// }
 // the 2 selections are then compared, and a winner is declared.
-function playRound(playerSelection,computerSelection){
-    // console.log(computerSelection); <-for testing
+// im not sure why the project page wanted the 2 selections to be parameters, it seems better to just have them inside the function?
+function playRound(){
+    let playerSelection = window.prompt('rock, paper, or scissors?');
     let input = playerSelection.toLowerCase();
 
     console.log(`You have chosen ${input}.`);
-    console.log(`The computer has chosen ${computerSelection}.`)
+
+    let computerSelection = computerPlay();
 
     if(input===computerSelection){
         console.log('It\'s a tie.');
-        return ;
+        return 0;
     }
     else if(input==='rock' && computerSelection==='paper'){
         console.log('Computer wins.');
-        return ;
+        return -1;
     }
     else if(input==='rock' && computerSelection==='scissors'){
         console.log('User wins.');
-        return ;
+        return 1;
     }
     else if(input==='paper' && computerSelection==='rock'){
         console.log('User wins.');
-        return ;
+        return 1;
     }
     else if(input==='paper' && computerSelection==='scissors'){
         console.log('Computer wins.');
-        return ;
+        return -1;
     }
     else if(input==='scissors' && computerSelection==='rock'){
         console.log('Computer wins.');
-        return ;
+        return -1;
     }
     else if(input==='scissors' && computerSelection==='paper'){
         console.log('User wins.');
-        return ;
+        return 1;
     }
     else{
         console.log('Invalid input.');
-        return ;
+        return 0;
     }
 }
-// playRound(playerSelection,computerSelection);
-
+let score = 0;
 // we now create a new function that plays 5 rounds and keeps score.
+// *** NEED TO CREATE SEPERATE SCORES FOR USER AND COMPUTER ***
 function game(){
-    // let playerScore = 0;
-    // let computerScore = 0;
-    playRound(playerSelection,computerSelection);
-    playRound(playerSelection,computerSelection);
-    playRound(playerSelection,computerSelection);
-    playRound(playerSelection,computerSelection);
-    playRound(playerSelection,computerSelection);
-
+    playRound() + score;
+    playRound() + score;
+    playRound() + score;
+    playRound() + score;
+    playRound() + score;
+    console.log(score);
+    if(score<3){
+        return console.log('Computer has won the best of 5')
+    }
+    else if(score>=3){
+        return console.log('You have won the best of 5!')
+    }
+    else{
+        return console.log('Outcome unknown.')
+    }
 }
 game();
 // play 5 games then stop
